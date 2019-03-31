197e9168aa173c818821201d3a7a8eb5066a43fc
Prakhar <prpatid2@in.ibm.com>
2019-03-31 17:02:55 +0530

    Java project

---
+++ src/JenkinsTestClass.java
@@ -0,0 +1,14 @@
+
+
+public class JenkinsTestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i=0; i< 10; i++) {
+			System.out.println("Number in loop is : " + i);
+		}
+
+	}
+
+}
